efe742f40bc253093e515f29d48657663daf52db
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-06-15 06:52:29 +0500

    learning

--- unique-values.js
+++ unique-values.js
@@ -2,11 +2,14 @@ function countUniqueValues(arr) {
 
     let i = 0;
     for (let j = 1; j < arr.length; j++) {
-
-
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+            console.log(arr);
+        }
+        console.log(i, j);
     }
-
 }
 
 
-[1, 1, 2, 3, 3, 4, 5, 6, 6, 7]
+countUniqueValues([1, 1, 1, 2, 2, 3, 4, 5, 5, 5, 6, 7]);
