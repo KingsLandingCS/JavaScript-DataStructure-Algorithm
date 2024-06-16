a59246979005c9f8b452eedce737eb1019c951bd
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-06-16 06:01:00 +0500

    learning

--- unique-values.js
+++ unique-values.js
@@ -1,16 +1,20 @@
 function countUniqueValues(arr) {
     if (arr.length === 0) return 0;
+
     let i = 0;
     for (let j = 1; j < arr.length; j++) {
         if (arr[i] !== arr[j]) {
             i++;
             arr[i] = arr[j];
-
+            console.log(arr);
         }
-
     }
     return i + 1;
 }
 
-
+                                                   
 console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 3]));
+                                                             
+
+// linear space complexity : o(1)
+// linear Time Complexity: o(n)
