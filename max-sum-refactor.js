f021f4a518ac6a4715e964cb16f960d2abcf5d27
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-06-17 19:38:31 +0500

    learning

--- max-sum-refactor.js
+++ max-sum-refactor.js
@@ -1,16 +1,27 @@
 function maxSubarraySum(arr, num) {
     let maxSum = 0;
     let tempSum = 0;
+
+    // Return null if the array is shorter than the number of elements to sum
     if (arr.length < num) return null;
+
+    // Calculate the sum of the first 'num' elements
     for (let i = 0; i < num; i++) {
-        maxSum += arr[i];
+        maxSum += arr[i];  // Add the current element to maxSum
     }
-    tempSum = maxSum;
+
+    tempSum = maxSum;  // Set the initial window sum to maxSum
+
+
+    // Slide the window across the array                                                
     for (let i = num; i < arr.length; i++) {
         tempSum = tempSum - arr[i - num] + arr[i];
-        maxSum = Math.max(maxSum, tempSum);
+        maxSum = Math.max(maxSum, tempSum);           // stroing max sum                                                                                                                      
     }
+
     return maxSum;
 }
 
 console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
+
+
---
+++ ntoes.js
@@ -0,0 +1,44 @@
+// arr[i - num],  arr[i]  
+//Element Sliding Out: arr[3 - 3] = arr[0] = 2
+// Element Sliding In: arr[3] = 2
+
+
+
+// First Slide (i = 3):
+
+// Previous Window: [2, 6, 9]
+// New Window: [6, 9, 2]
+// Element Sliding Out: arr[3 - 3] = arr[0] = 2
+// Element Sliding In: arr[3] = 2
+// Update tempSum: tempSum = tempSum - arr[0] + arr[3]
+// Calculation: tempSum = 17 - 2 + 2 = 17
+
+
+// Second Slide (i = 4):
+
+// Previous Window: [6, 9, 2]
+// New Window: [9, 2, 1]
+// Element Sliding Out: arr[4 - 3] = arr[1] = 6
+// Element Sliding In: arr[4] = 1
+// Update tempSum: tempSum = tempSum - arr[1] + arr[4]
+// Calculation: tempSum = 17 - 6 + 1 = 12
+
+// Slide Right (i = 6):
+
+// New Window: [1, 8, 5]
+// Remove arr[3] = 2 (element sliding out).
+// Add arr[6] = 5 (element sliding in).
+// tempSum = tempSum - arr[3] + arr[6]
+// tempSum = 11 - 2 + 5 = 14
+// Update maxSum = Math.max(17, 14) = 17
+
+
+
+// Slide Right (i = 8):
+
+// New Window: [5, 6, 3]
+// Remove arr[5] = 8 (element sliding out).
+// Add arr[8] = 3 (element sliding in).
+// tempSum = tempSum - arr[5] + arr[8]
+// tempSum = 19 - 8 + 3 = 14
+// Update maxSum = Math.max(19, 14) = 19
