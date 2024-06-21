f2234ac60f55d8e96f7fdc498bedc184556e7227
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-06-21 17:29:29 +0500

    learning

--- count-down.js
+++ count-down.js
@@ -10,3 +10,18 @@ function countDown(num) {
 }
 
 countDown(5);
+
+
+
+//print 5
+//countDown(5)
+//print 4
+//countDown(4)
+//print 3
+//countDown(3)
+//print 2
+//countDown(2)
+//print 1
+//countDown(1)
+//print 0
+//countDown(0)          ==> 0 - 1 = -1, ==> num is less 0, so, it will print "All done"
