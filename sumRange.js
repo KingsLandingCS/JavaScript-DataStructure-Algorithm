477cb0234e53ce3464a398ac56433d1335bdd79e
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-06-24 22:11:51 +0500

    learning

---
+++ sumRange.js
@@ -0,0 +1,12 @@
+function sumRange(num) {
+    if (num === 1) return 1;
+    return num + sumRange(num - 1);
+}
+
+console.log(sumRange(3));
+
+// return 3 + 3;  ==> 6
+
+//return 3 + sumRange(2)                ==> 3     
+            //  return 2 + sumRange(1)  ==> sumRange = 2 + 1
+                         //  return 1;  ==> sumRange = 1
