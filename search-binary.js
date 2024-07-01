9764211cd5dfab739c194b47fddc114610b49f76
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-07-01 22:37:31 +0500

    learning

--- search-binary.js
+++ search-binary.js
@@ -5,8 +5,7 @@ function binarySearch(arr, elem) {
     let middle = Math.floor((start + end) / 2);
 
 
-    while (arr[middle] !== elem) {
-        console.log(start, middle, end);
+    while (arr[middle] !== elem && start <= end) {
         if (elem < arr[middle]) {
             end = middle - 1;
         } else {
@@ -14,8 +13,12 @@ function binarySearch(arr, elem) {
         }
         middle = Math.floor((start + end) / 2);
     }
-    console.log(start, middle, end);
-    return middle;
+    return arr[middle] === elem ? middle : -1;
+
+    // if (arr[middle] == elem) {
+    //     return middle;
+    // }
+    // return -1;
 }
 
 console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 28));
