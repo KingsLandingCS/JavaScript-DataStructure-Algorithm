61b7fbfc6d8d6f832c9939c7bb9c678a47b6c2c6
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-07-06 15:48:24 +0500

    learning

---
+++ bubble-sort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                //SWAP!
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+            console.log(arr);
+        }
+        console.log(`After ${(i + 1)} iteration of i: ${arr}`);
+    }
+    return arr;
+}
+
+bubbleSort([37, 45, 29, 8]);
