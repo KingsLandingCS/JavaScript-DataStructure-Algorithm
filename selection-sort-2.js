9ff3c8e9fae4dc6c28fa962852ce0f3a9a09d00d
Zubair UnderWood <finalfantasy.fantasy9@gmail.com>
2024-07-12 20:38:22 +0500

    learning

--- selection-sort-2.js
+++ selection-sort-2.js
@@ -9,8 +9,6 @@ function selectionSort(arr) {
             }
             console.log(`Inner loop comparison: [${arr}], checking if value of j greater or lower ${arr[j]}, ${arr[lowest]}, smallest value at index: ${lowest}\n`);
         }
-
-
         if (i !== lowest) {         // ==> Avoid Unnecessary Swaps: If the smallest element in the unsorted portion of the array is already in the position i, swapping it with itself would be redundant and wasteful.The check if (i !== lowest) ensures that we only perform a swap if the smallest element is not already at the current position i.
             console.log(`${i}, ${lowest}\n`);
             let temp = arr[i];
