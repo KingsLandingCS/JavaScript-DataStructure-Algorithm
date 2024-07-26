4f28b61a34ed80917a4d41e60748569aca1f2514
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-07-26 16:30:39 +0500

    learning

---
+++ quick-sort.js
@@ -0,0 +1,38 @@
+function pivot(arr, start = 0, end = arr.length - 1) {    //start = 0: This means that if the caller does not provide a value for start, it will default to 0.
+    // end = arr.length - 1: This means that if the caller does not provide a value for end, it will default to arr.length - 1, which is the last index of the array arr.
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    }
+
+    // We are assuming the pivot is always the first element
+    let pivot = arr[start];
+    let swapIdx = start;
+
+    console.log(`Orginal Array: ${arr}`);
+    for (let i = start + 1; i <= end; i++) {
+        console.log(`Iteration ${i}: ${arr}`);
+        if (pivot > arr[i]) {
+            swapIdx++;
+            console.log(`Swap: ${swapIdx} and ${i}`);
+            swap(arr, swapIdx, i);
+            console.log(`Results of loop at iteration ${i}: ${arr}`);
+        }
+    }
+    // Swap the pivot from the start the swapPoint
+    swap(arr, start, swapIdx); // the swapIdx is 3 here because of the manipulation in for loop above, and arr will updated too. Start will be 0 though. There will be final swap call to update the pivot position.
+    // return `final array: ${arr} Pivot: ${pivot} SwapIdx: ${swapIdx}`;
+    return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right)
+        //left
+        quickSort(arr, left, pivotIndex - 1);
+        //right
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
+
+console.log(quickSort([4, 8, 2, 1, 5, 7, 6, 3]));
