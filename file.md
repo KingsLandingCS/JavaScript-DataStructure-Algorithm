8887d705585d7b706c3ee034487f67d79762bcd1
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-03 23:29:28 +0500

    learning

---
+++ file.md
@@ -0,0 +1,8 @@
+#Markdown
+
+student : {
+    age: 21,
+    sex:male,
+    major: science,
+    year:2nd
+}
---
+++ singly-linked-list.js
@@ -0,0 +1,28 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        
+    }
+}
+
+// var first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
+
+
+let list = new SinglyLinkedList();
+list.push("Hello");
+list.push("GoodBye");
