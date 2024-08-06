8290f634d6ea5729716242a4c7a2bd2becc9a7ef
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-08-07 00:49:53 +0500

    learning

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -23,11 +23,39 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+    // traverse() {
+    //     let current = this.head;
+    //     while (current) {
+    //         console.log(current.val);
+    //         current = current.next;
+    //     }
+    // }
+
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
 }
 
 
 let list = new SinglyLinkedList();
 list.push("Hello");
 list.push("GoodBye");
+list.push("!");
+// list.traverse();
+list.pop();
 
 console.log(list);
